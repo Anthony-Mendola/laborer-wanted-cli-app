eddc3b2f645321bf29f925cd9587414a8c042f5e
Anthony <github email address>
2018-01-21 16:21:32 -0500

    having issues with getting all listings on page

--- README.md
+++ README.md
@@ -6,27 +6,16 @@ TODO: Delete this and the text above, and describe your gem
 
 ## Installation
 
-Add this line to your application's Gemfile:
+You can install this gem via `gem install laborer_wanted`. The `laborer-wanted` CLI will be installed and you can run `laborer-wanted` to get a list of movies playing now right in your command line.
 
-```ruby
-gem 'laborer_wanted'
-```
-
-And then execute:
-
-    $ bundle
-
-Or install it yourself as:
-
-    $ gem install laborer_wanted
 
 ## Usage
 
-TODO: Write usage instructions here
+Run: `laborer-wanted` after installing the gem.
 
 ## Development
 
-After checking out the repo, run `bin/setup` to install dependencies. Then, run `rake spec` to run the tests. You can also run `bin/console` for an interactive prompt that will allow you to experiment.
+After checking out the repo, run `bundle exec bin/setup` to install dependencies. Then, run `bundle exex rake spec` to run the tests. You can also run `bundle exec bin/console` for an interactive prompt that will allow you to experiment.
 
 To install this gem onto your local machine, run `bundle exec rake install`. To release a new version, update the version number in `version.rb`, and then run `bundle exec rake release`, which will create a git tag for the version, push git commits and tags, and push the `.gem` file to [rubygems.org](https://rubygems.org).
 
--- lib/laborer_wanted/laborer.rb
+++ lib/laborer_wanted/laborer.rb
@@ -16,13 +16,22 @@ class LaborerWanted::Laborer
   end
 
   def self.scrape_craigslist
-    doc = Nokogiri::HTML(open("https://newyork.craigslist.org/search/res?query=construction+laborer"))
-
+  #  doc = Nokogiri::HTML(open("https://newyork.craigslist.org/search/res?query=construction+laborer"))
+    doc = HTTParty.get("https://newyork.craigslist.org/search/res?query=construction+laborer"))
     laborer = self.new
+    parse_doc = Nokogiri::HTML(doc)
+    laborer_array = []
+   parse_doc.css('.content').css('.row').css('.hdrlnk').map do |a|
+     listing_name = a.text
+     laborer_array.push(list_name)
+   end
+binding.pry
     laborer.name = doc.css("a.result-title.hdrlnk").first.text
+
     laborer.date = doc.css("time.result-date").first.text
     laborer.location = doc.css("span.result-hood").first.text
     laborer.url = doc.css("a.result-title.hdrlnk").first.attr("href")
+
     laborer
   end
 
@@ -34,6 +43,11 @@ class LaborerWanted::Laborer
     laborer.date = doc.css("span.last_updated").first.text
     laborer.location = doc.css("span.location").first.text
     laborer.url = doc.css("a.app_link").first.attr("href")
+
     laborer
   end
 end
+
+#name = doc.css("a[class='result-title hdrlnk']").first.text
+#name = doc.search("p[class='result-info'] a[class='result-title hdrlnk']")
+#name.collect{|e| new(e.text.strip, "https://newyork.craigslist.org/#{e.attr("href").split("?").first.strip}")}
