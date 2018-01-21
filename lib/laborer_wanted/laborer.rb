9db6b7012388e28f1f0398f04984fe39bab10036
Anthony <github email address>
2018-01-20 19:47:37 -0500

    refined scraper code

--- lib/laborer_wanted/laborer.rb
+++ lib/laborer_wanted/laborer.rb
@@ -7,19 +7,14 @@ class LaborerWanted::Laborer
   end
 
   def self.scrape_laborers
-    laborer_1 = self.new
-    laborer_1.name = "High end carpenter and tile expert"
-    laborer_1.availability = "Available"
-    laborer_1.contact = "(347) 512-3863"
-    laborer_1.url ="https://newyork.craigslist.org/brk/res/d/high-end-carpenter-and-tile/6465087259.html"
+    laborers = []
+    # Go to craigslist, find the laborers
+    # Extract the properties
+    # Instantiate a laborer listing
 
-    laborer_2 = self.new
-    laborer_2.name = "Handyman/Construction/Worker"
-    laborer_2.availability = "Available"
-    laborer_2.contact = "dj35v-6447093851@res.craigslist.org"
-    laborer_2.url ="https://newyork.craigslist.org/que/res/d/handyman-construction-worker/6447093851.html"
+    # Go to indeed
 
-    [laborer_1, laborer_2]
+   laborers
   end
 
 end
