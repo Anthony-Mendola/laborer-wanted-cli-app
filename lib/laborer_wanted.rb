a9ccf8050a163bcbe23109b3e927d461f8784bf5
Anthony <github email address>
2018-01-30 14:27:52 -0500

    gem installed

--- lib/laborer_wanted.rb
+++ lib/laborer_wanted.rb
@@ -2,7 +2,7 @@ require 'open-uri'
 require 'nokogiri'
 require 'pry'
 
-require_relative "laborer_wanted/version"
+require_relative './laborer_wanted/version'
 require_relative './laborer_wanted/cli'
 require_relative './laborer_wanted/laborer'
 require_relative './laborer_wanted/scraper'
